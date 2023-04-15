f6e1cdded6aad069f3fd323db3d77214783f34e6
Mesele <getmesy@gmail.com>
2023-04-15 15:09:25 +0000

    correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from fabric.api import local
+from time import strftime
 from datetime import datetime
+
 
 def do_pack():
     """ a Fabric script that generates a .tgz archive from the contents
     of the web_static folder of your AirBnB Clone rep"""
+    time_stamp = strftime("%Y%m%d%H%M%S")
     try:
-        time_stamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
-        file_path = "versions/web_static_{}.tqz".format(time_stamp)
-        local("tar -cvzf{} web_static".format(file_path))
-        return file_path
+        local("tar -cvvf versions/web_static_{}.tgz web_static/".format(time_stamp))
+        return "versions/web_static_{}.tgz".format(time_stamp)
     except Exception as e:
         return None
