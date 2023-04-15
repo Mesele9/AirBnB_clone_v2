0e3c119504d253dde70fd69e9587e51da0687dab
Mesele <getmesy@gmail.com>
2023-04-15 15:15:06 +0000

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,8 @@ def do_pack():
     time_stamp = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -cvvf versions/web_static_{}.tgz web_static/".format(time_stamp))
+        local("tar -cvvf versions/web_static_{}.tgz web_static/"
+              .format(time_stamp))
         return "versions/web_static_{}.tgz".format(time_stamp)
     except Exception as e:
         return None
