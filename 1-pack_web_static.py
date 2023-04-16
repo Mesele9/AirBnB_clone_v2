334169c5a865fd5193d3f80df7ae2e6f9a79199c
Mesele <getmesy@gmail.com>
2023-04-16 15:23:07 +0000

    correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     time_stamp = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -cvvf versions/web_static_{}.tgz web_static/"
+        local("tar -cvzf versions/web_static_{}.tgz web_static/"
               .format(time_stamp))
         return "versions/web_static_{}.tgz".format(time_stamp)
     except Exception as e:
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from os import path
 
 env.hosts = ['54.80.197.65', '54.157.160.175']
 env.user = 'ubuntu'
-env.my_ssh_private_key = '~/.ssh/id_rsa'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -19,26 +19,23 @@ def do_deploy(archive_path):
 
         put(archive_path, '/tmp/')
 
-        time_stamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
-            .format(time_stamp))
+        archive_name = path.basename(archive_path)
+        archive_basename = path.splitext(archive_name)[0]
+        release_path = '/data/web_static/releases/{}'.format(archive_basename)
+        run('sudo mkdir -p {}'.format(release_path))
 
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/\
-                releases/web_static_{}/'.format(time_stamp, time_stamp))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(archive_name, release_path))
 
-        run('sudo rm /tmp/web_static_{}.tgz'.format(time_stamp))
+        run('sudo rm /tmp/{}'.format(archive_name))
 
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/*\
-            /data/web_static/releases/web_static_{}/'
-            .format(time_stamp, time_stamp))
+        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_basename, archive_basename))
 
-        run('sudo rm -rf /data/web_static/release/\
-            web_static_{}/web_static'.format(time_stamp))
+        run('sudo rm -rf /data/web_static/release/{}/web_static'.format(archive_basename))
 
         run('sudo rm -rf /data/web_static/current')
 
-        run('sudo ln -s /data/web_static/releases/\
-            web_static_{}/ /data/web_static/current'.format(time_stamp))
+        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_basename))
+
     except:
         return False
 
